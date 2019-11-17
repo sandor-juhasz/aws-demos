ef2d3ea291fddf178ca0c121f23d9fd75028a4b1
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2019-11-17 09:35:08 +0100

    Updating comments

--- serverless/protected-api/src/vagrant/bootstrap.sh
+++ serverless/protected-api/src/vagrant/bootstrap.sh
@@ -6,29 +6,28 @@
 
 sudo adduser vagrant vboxsf
 apt-get update
-apt-get install -y jq python-pip zip
-apt-get install -y python3 python3-pip
 
-pip install awscli
-
+################################################################################
+# Installation steps for AWS CLI and the SAM CLI (including Docker)
 #
-# Installation steps for SAM CLI
 # NOTE: As of 2019-11-17, the official AWS Serverless Application Model
 #       developer guide still recommends installing the SAM CLI with Homebrew.
 #       A simple pip install aws-sam-cli works perfectly and does not bloat
 #       the system with additional redundant downloads like a whole Python3
 #       environment.
 #
+# References:
+#   - https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install-linux.html
+#   - https://github.com/awslabs/aws-sam-cli/issues/1424
+#
+################################################################################
+apt-get install -y jq python-pip zip
+pip install awscli
 apt-get install -y docker.io
 usermod -aG docker vagrant
 pip install aws-sam-cli
 
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-#test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-#test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-#test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-#echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
-
-
-
-
+################################################################################
+# Installing Python3 for project dependent development.
+################################################################################
+apt-get install -y python3 python3-pip
