a0813d7054baf9ce9681acf607552408b8a5ac3c
sandor-juhasz <sandor.juhasz.1983@gmail.com>
2019-11-17 11:24:06 +0100

    Updating provisioning script to install Python 3.7.

--- serverless/protected-api/src/vagrant/bootstrap.sh
+++ serverless/protected-api/src/vagrant/bootstrap.sh
@@ -13,12 +13,18 @@ apt-get update
 # NOTE: As of 2019-11-17, the official AWS Serverless Application Model
 #       developer guide still recommends installing the SAM CLI with Homebrew.
 #       A simple pip install aws-sam-cli works perfectly and does not bloat
-#       the system with additional redundant downloads like a whole Python3
-#       environment.
+#       the system with additional redundant downloads like a whole Python3.7
+#       environment. The Python3.7 environment is still needed to build and test
+#       the Python3.7 Lambda functions.
 #
 # NOTE: The SAM CLI does not support installations on Python 2.7. When installed
 #       on a Python 2.7 runtime, a red error message is displayed guiding the user
 #       to follow the official Homebrew-based installation procedure.
+#
+# NOTE: The Python runtime matching the Lambda runtimes should be installed on
+#       the Vagrant VM. In this case, Python 3.7. The default Python 3
+#       implementation on Ubuntu Bionic is Pyhton 3.6.8 and it is mandatory to
+#       support APT and other Linux tools.
 #
 # References:
 #   - https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/serverless-sam-cli-install-linux.html
@@ -27,7 +33,7 @@ apt-get update
 ################################################################################
 #apt-get install -y jq python-pip zip
 
-apt-get install -y python3 python3-pip
+apt-get install -y python3 python3-pip python3.7
 apt-get install -y docker.io
 usermod -aG docker vagrant
 pip3 install awscli
